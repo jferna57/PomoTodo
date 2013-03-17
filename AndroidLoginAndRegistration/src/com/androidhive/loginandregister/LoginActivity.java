cb149edaca54485bd6225454e9545e5e7e922b87
jcfernandez <juan.carlos.fernandez@ricoh.es>
2013-03-17 17:38:55 +0100

    add more source code

---
+++ AndroidLoginAndRegistration/src/com/androidhive/loginandregister/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.androidhive.loginandregister;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+        
+        // Listening to register new account link
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// Switching to Register screen
+				Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+				startActivity(i);
+			}
+		});
+    }
+}
---
+++ AndroidLoginAndRegistration/src/com/androidhive/loginandregister/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.androidhive.loginandregister;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set View to register.xml
+        setContentView(R.layout.register);
+        
+        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+        
+        // Listening to Login Screen link
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				// Switching to Login Screen/closing register screen
+				finish();
+			}
+		});
+    }
+}
